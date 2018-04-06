2136eda5877d77e998836ef1bd14ad4d893c365f
Dan Martin <dan@dleo.info>
2018-04-05 18:15:50 -0700

    chapter 35

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -27,19 +27,30 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+    handlePick() {
+        alert('handlePick');
+    }
     render() {
         return (
             <div>
-                <button>What should I do?</button>
+                <button onClick={this.handlePick}>What should I do?</button>
             </div>
         );
     }
 }
 
 class Options extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
+    handleRemoveAll() {
+        console.log(this.props.options);
+    }
     render () {
         return (
             <div>
+                <button onClick={this.handleRemoveAll}>Remove All</button>
                 {
                     this.props.options.map((option) => <Option key={option} optionText={option} />)
                 }
@@ -59,9 +70,24 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+    handleAddOption(e) {
+        e.preventDefault();
+        const option = e.target.elements.option.value.trim();
+
+        if(option) {
+            alert(option);
+            e.target.elements.option.value = '';
+        } else {
+            alert('I don\'t think so!');
+        }
+    }
     render() {
+        console.log(this);
         return (
-            <p>Add Option component here</p>
+            <form onSubmit={this.handleAddOption}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
         );
     }
 }
--- indecision-app/src/playground/build-it-visible.js
+++ indecision-app/src/playground/build-it-visible.js
@@ -1,29 +1,63 @@
-const appObject = {
-    title: 'Visibility Toggle',
-    subtitle: 'Hey. These are some details you can now see.',
-    options: []
-};
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility: false
+        }
+    }
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            };
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>
+                    {this.state.visibility ? 'Hide ' : 'Show '} details
+                </button>
+                {this.state.visibility &&
+                    <div>
+                        <p>Hey. These are some details you can now see!</p>
+                    </div>
+                }
+            </div>
+        );
+    }
+}
 
-let visible = false;
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
 
-const onToggleVisibility = () => {
-    visible = !visible;
-    console.log(visible);
-    render();
-};
+// const appObject = {
+//     title: 'Visibility Toggle',
+//     subtitle: 'Hey. These are some details you can now see.',
+//     options: []
+// };
 
-const appRoot = document.getElementById('app');
+// let visible = false;
 
-const render = () => {
-    const template = (
-        <div>
-            <h1>{appObject.title}</h1>
-            <button onClick={onToggleVisibility}>{(visible) ? 'Hide' : 'Show'} details</button>
-            {visible && <p>{appObject.subtitle}</p>}
-        </div>
-    );
+// const onToggleVisibility = () => {
+//     visible = !visible;
+//     console.log(visible);
+//     render();
+// };
 
-    ReactDOM.render(template, appRoot);
-};
+// const appRoot = document.getElementById('app');
 
-render();
+// const render = () => {
+//     const template = (
+//         <div>
+//             <h1>{appObject.title}</h1>
+//             <button onClick={onToggleVisibility}>{(visible) ? 'Hide' : 'Show'} details</button>
+//             {visible && <p>{appObject.subtitle}</p>}
+//         </div>
+//     );
+
+//     ReactDOM.render(template, appRoot);
+// };
+
+// render();
