6d1c3a0b0db6b4965cb418dfa1cd10332d4be6ee
Dan Martin <dmartin@fh.org>
2018-04-09 20:53:25 -0700

    chapter 53

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,13 +1,5 @@
-import { square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is running!');
-console.log(square(5));
-console.log(add(100, 23));
- 
-import isSenior, {isAdult, canDrink} from './person';
-
-const age = 19;
-
-console.log('isAdult', isAdult(age));
-console.log('canDrink', canDrink(age));
-console.log('isSenior', isSenior(age));
+const template = React.createElement('p', {}, 'testing 1,2,3');
+ReactDOM.render(template, document.getElementById('app'));
